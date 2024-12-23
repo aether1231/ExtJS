7ee592fed4e4eb1ee5ef1e16135884fcfede49a6
Kevin <tlk999@live.co.uk>
2024-12-23 12:59:28 +0000

    Combobox

--- js/app.js
+++ js/app.js
@@ -1,21 +1,7 @@
 Ext.require(["Ext.button.Button", "Ext.panel.Panel", "Ext.toolbar.Toolbar"]);
 
 Ext.onReady(() => {
-  // modalWindow();
-  // messageBox1();
-  // messageBox2();
-  // tabPanel();
-  // layoutAuto();
-  // layoutAnchor();
-  // layoutAnchor2();
-  // layoutFit();
-  // layoutAccordion();
-  // layoutCard();
-  // layoutColumn();
-  // layoutHbox();
-  // layoutVbox();
-  // layoutTable();
-  // layoutTable2();
-  // layoutBorder();
-  layoutBorder2();
+  // textField1();
+  // numberField1();
+  combobox1();
 });
---
+++ js/forms.js
@@ -0,0 +1,134 @@
+const combobox1 = () => {
+  Ext.QuickTips.init();
+  var remoteJsonStore = Ext.create(Ext.data.Store, {
+    root: "records",
+    totalProperty: "totalCount",
+    baseParams: {
+      column: "fullName",
+    },
+    fields: ["fullName", "id"],
+    proxy: {
+      type: "jsonp",
+      url: "http://extjsinaction.com/dataQuery.php",
+    },
+  });
+
+  var combo = {
+    xtype: "combo",
+    fieldLabel: "Search by name",
+    forceSelection: true,
+    displayField: "name",
+    loadingText: "Querying....",
+    minChars: 1,
+    store: remoteJsonStore,
+  };
+
+  new Ext.Window({
+    title: "",
+    height: 100,
+    labelWidth: 100,
+    bodyStyle: "padding: 5px",
+    items: combo,
+  }).show();
+};
+
+const numberField1 = () => {
+  Ext.QuickTips.init();
+
+  var fpItems = [
+    {
+      xtype: "numberfield",
+      fieldLabel: "Numbers only",
+      allowBlank: false,
+      emptyText: "This field is empty!",
+      decimalPrecision: 3,
+      minValue: 0.001,
+      maxValue: 2,
+    },
+  ];
+
+  var fp = new Ext.form.Panel({
+    renderTo: Ext.getBody(),
+    width: 400,
+    title: "Exercising textfields",
+    height: 170,
+    frame: true,
+    bodyStyle: "padding: 5px",
+    monitorValid: true,
+    monitorPoll: 50,
+    labelWidth: 125,
+    defaults: {
+      msgTarget: "side",
+      anchor: "-20",
+    },
+    items: fpItems,
+  });
+};
+
+const textField1 = () => {
+  Ext.QuickTips.init();
+
+  var fpItems = [
+    {
+      fieldLabel: "Alpha only",
+      allowBlank: false,
+      emptyText: "This field is empty!",
+      maskRe: /[a-z]/i,
+      msgTarget: "side",
+    },
+    {
+      fieldLabel: "Simple 3 to 7 Chars",
+      allowBlank: false,
+      msgTarget: "title",
+      minLength: 3,
+      maxLength: 7,
+    },
+    {
+      fieldLabel: "Special Chars Only",
+      allowBlank: false,
+      msgTarget: "qtip",
+      stripCharsRe: /[a-zA-Z0-9]/gi,
+    },
+    {
+      fieldLabel: "Web Only with VType",
+      allowBlank: false,
+      vtype: "url",
+      msgTarget: "side",
+    },
+    {
+      fieldLabel: "Password",
+      allowBlank: false,
+      inputType: "password",
+    },
+    {
+      fieldLabel: "File",
+      allowBlank: false,
+      xtype: "filefield",
+    },
+    {
+      xtype: "textarea",
+      fieldLabel: "My TextArea",
+      name: "myTextArea",
+      anchor: "100%",
+      height: 100,
+    },
+  ];
+
+  var fp = Ext.create("Ext.form.Panel", {
+    renderTo: Ext.getBody(),
+    width: 400,
+    title: "Exploring text fields",
+    //            height       : 220,
+    frame: true,
+    bodyStyle: "padding: 5px",
+    monitorValid: true,
+    monitorPoll: 50,
+    labelWidth: 125,
+    defaultType: "textfield",
+    defaults: {
+      msgTarget: "side",
+      anchor: "-20",
+    },
+    items: fpItems,
+  });
+};
